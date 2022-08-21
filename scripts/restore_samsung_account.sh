a00a3053237fc66f638474b7673ffd2e2a5f5563
Jan Biernacik <contact@janbiernacik.com>
2022-08-21 20:24:01 +0200

    Delete scripts directory

--- scripts/restore_samsung_account.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/bash
-clear
-echo 'Restoring Samsung Account...'
-echo '----------------------------'
-adb shell pm install-existing com.osp.app.signin
-adb shell pm install-existing com.samsung.klmsagent
